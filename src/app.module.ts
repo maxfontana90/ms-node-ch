5e837ef97b614d6b97e401daf0a00256a78f4896
Maximiliano A. Fontana <fontana@csdisco.com>
2020-07-12 19:05:02 -0300

    feat: 🎸 Adding Posts & Authors resources
    Adding Posts & Authors resources and their initial scaffolding

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsController } from './modules/posts/controller/posts.controller';
+import { AuthorsController } from './modules/authors/controller/authors.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, PostsController, AuthorsController],
   providers: [AppService],
 })
 export class AppModule {}

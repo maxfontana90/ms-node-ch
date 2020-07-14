27e7fdc4981a4813bc71121b0748d2a7c6206fa2
Maximiliano A. Fontana <fontana@csdisco.com>
2020-07-14 13:21:34 -0300

    refactor: 💡 Deleting unneeded default-autogenerated nest assets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { AuthorsModule } from './modules/authors/authors.module';
 import { DataAccessModule } from './modules/data-access/data-access.module';
 import { PostsModule } from './modules/posts/posts.module';
@@ -13,8 +12,6 @@ import { AuthModule } from './modules/auth/auth.module';
     DataAccessModule,
     AuthModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {
 }
